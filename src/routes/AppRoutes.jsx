376488d40ba2bdcad2b95f365087e9d68f5fd80e
Shabbir Umrali <shabbirumrali@gmail.com>
2026-02-07 16:43:28 +0530

    Added - AppRoutes.jsx with all routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,35 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+const Home = lazy(() => import ('../pages/Home/Home'));
+const Contact = lazy(() => import ('../pages/ContactUs/ContactUs'));
+const Services = lazy(() => import ('../pages/Services/Services'));
+const RequestCallback = lazy(() => import ('../pages/RequestCallback/RequestCallback'));
+const Maintenance = lazy(() => import ('../pages/Maintenance/Maintenance'));
+const NotFound = lazy(() => import ('../pages/NotFound/NotFound'));
+
+const MainLayout = lazy(() => import ('../components/layout/MainLayout'));
+
+
+// AppRoutes - central routing configuration
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<div className='flex items-center justify-center min-h-screen'>loading...</div>}>
+                <Routes>
+                    {/* <Route element={<MainLayout />}> */}
+                        {/* <Route path='/' element={<Home />} />
+                        <Route path='/services' element={<Services />} />
+                        <Route path='/contact' element={<Contact />} />
+                        <Route path='/request-callback' element={<RequestCallback />} /> */}
+                        <Route path='*' element={<Maintenance />} />
+                        {/* <Route path='*' element={<NotFound />} /> */}
+
+                        {/* <Route path="/home" element={<Navigate to="/" replace/>} />
+                        <Route path="/callback" element={<Navigate to="/request-callback" replace/>} /> */}
+                    {/* </Route> */}
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
+}
